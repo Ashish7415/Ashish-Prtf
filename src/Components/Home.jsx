d1fe98a191c3778d8921b1b201d5c4265611b0f4
ashishpatel0981@gmail.com <ashishpatel0981@gmail.com>
2024-08-25 18:30:14 +0530

    add resume

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ import { ReactTyped } from "react-typed";
 
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-scroll";
-import Resume from "../../public/Resume.pdf"
+import AshishResume from '../assets/AshishResume.pdf'
 
 
 
@@ -73,7 +73,7 @@ function Home() {
               <div className=" space-y-2">
                   {/* resume parts*/}
                 <div>
-                <a href="#" className=' bg-blue-600 px-20 py-3 rounded-md text-xl' download>
+                <a href={AshishResume} className=' bg-blue-600 px-20 py-3 rounded-md text-xl' download>
                     resume
                 </a>
             </div>                       
